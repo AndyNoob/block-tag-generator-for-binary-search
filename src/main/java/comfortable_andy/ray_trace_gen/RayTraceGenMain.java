6cd485b0e0dd701be22f863430e05893feeb779f
AndyNoob <comfortableandy9082@gmail.com>
2024-07-06 19:09:06 -0700

    Initial steps to loading minecraft on runtime

--- src/main/java/comfortable_andy/ray_trace_gen/RayTraceGenMain.java
+++ src/main/java/comfortable_andy/ray_trace_gen/RayTraceGenMain.java
@@ -4,11 +4,16 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.Strictness;
 import comfortable_andy.ray_trace_gen.accessors.BlockAccessor;
+import org.apache.commons.cli.*;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public final class RayTraceGenMain {
@@ -19,7 +24,22 @@ public final class RayTraceGenMain {
             .disableHtmlEscaping()
             .create();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
+        Option minecraftLoc = Option.builder("m")
+                .longOpt("minecraft")
+                .required()
+                .desc("Minecraft client jar file location")
+                .hasArg()
+                .build();
+        CommandLine cl = new DefaultParser().parse(
+                new Options().addOption(minecraftLoc),
+                args
+        );
+        final File file = new File(cl.getOptionValue(minecraftLoc));
+        try (URLClassLoader loader = new URLClassLoader(new URL[]{file.toURI().toURL()})) {
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private void generate(String name, List<String> mats) {
@@ -78,7 +98,7 @@ public final class RayTraceGenMain {
 
     public static record BlockTag(List<String> values) {
     }
-    
+
     public record Pair<A, B>(A first, B second) {
         public static <C, D> Pair<C, D> of(C c, D d) {
             return new Pair<>(c, d);
